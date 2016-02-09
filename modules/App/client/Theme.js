4eb81bc139aa6c9f90971b5650f3a8b825d233e3
Lucas Silva <lsilvawd09@gmail.com>
2016-02-09 00:29:49 -0500

    fine tuned color scheme

--- modules/App/client/Theme.js
+++ modules/App/client/Theme.js
@@ -12,7 +12,7 @@ export default {
         // #5c6bc0
         primary1Color: Colors.indigo400,
         primary2Color: Colors.deepPurple400,
-        primary3Color: Colors.yellow50,
+        primary3Color: Colors.white,
         accent1Color: Colors.yellow500,
         accent2Color: Colors.indigo600,
         accent3Color: Colors.grey500,
