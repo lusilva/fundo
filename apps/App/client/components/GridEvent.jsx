4854605acf816713259ec74d33d3fe6e26eac0aa
galarodo <marshall.shortledge@gmail.com>
2016-03-31 21:02:51 -0400

    Created default image
    Added default image for events.

--- apps/App/client/components/GridEvent.jsx
+++ apps/App/client/components/GridEvent.jsx
@@ -47,7 +47,8 @@ export default class GridEvent extends BaseEvent {
         }
 
         //TODO: change this to be placeholder image based on category.
-        let eventImage = "http://semantic-ui.com/images/avatar/large/elliot.jpg";
+        // let eventImage = "http://semantic-ui.com/images/avatar/large/elliot.jpg";
+        let eventImage = "../img/fundo-default-event-img.png";
         // First check if event has a medium image, if not then check if it has a large image.
         if (event.image && event.image.large) {
             eventImage = event.image.large.url;
